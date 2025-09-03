b9b00bac20945238b20c495eaffcdbd4126feca1
Juan David <juandavidcarrillogarcia@gmail.com>
2025-09-02 20:46:34 -0500

    se trabajo en el input del header, se instalo la libreria de icono font answer para ir mejorando la maquetacion en el header

--- src/app/Pages/home/home/home.component.ts
+++ src/app/Pages/home/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 // Importar Componentes
 import { HeaderComponent } from '../../../components/header/header/header.component';
-
+import { AsideComponent } from '../../../components/aside/aside/aside.component';
+import { MainComponent } from '../../../components/main/main/main.component';
+import { NgStyle } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, AsideComponent, MainComponent, NgStyle],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
