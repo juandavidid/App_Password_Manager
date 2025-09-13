15f5bd6727497fea925a0791103428d9c584f59e
Juan David <juandavidcarrillogarcia@gmail.com>
2025-09-12 23:34:35 -0500

    Declaracion  Rutas login home App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+// Importar Componentes
+import { HomeComponent } from './Pages/home/home/home.component';
+import { LoginComponent } from './Pages/login/login/login.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // DECLARAMOS RUTAS
+  { path: 'home/:valueBooleans', component: AppComponent },
+  { path: 'login', component: LoginComponent },
+];
