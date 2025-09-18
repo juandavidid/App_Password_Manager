54dd8b5c136edc3140d69d21f8b37f746fdd3ad2
Juan David <juandavidcarrillogarcia@gmail.com>
2025-09-18 16:53:46 -0500

    Menu: Maquetado del Menu

--- src/app/components/header/header/header.component.ts
+++ src/app/components/header/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NgStyle } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [NgStyle],
+  imports: [NgStyle, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  menuOpen: boolean = false;
+  displayIcon: boolean = true;
+  userName: string = 'Juan David';
+
+  // Alternar el menu al hacer clic en el icono
+  toggleMenu(event: MouseEvent) {
+    event.stopPropagation(); //Evita que el clic cierre el menu inmediatamente
+    this.menuOpen = !this.menuOpen;
+    this.displayIcon = !this.displayIcon;
+  }
+  // Detectar clic fuera del menú
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: Event) {
+    this.menuOpen = false;
+    this.displayIcon = true;
+  }
+}
